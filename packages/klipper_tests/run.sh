8efc9c3afc755a2ee386b4a50ba348b48501b8be
reemo3dp <150130755+reemo3dp@users.noreply.github.com>
2024-08-13 20:22:19 +0200

    Use docker compose

--- packages/klipper_tests/run.sh
+++ packages/klipper_tests/run.sh
@@ -5,17 +5,17 @@ set -euxo pipefail
 trap destroy EXIT
 
 destroy() {
-    docker-compose down -t0 || true
+    docker compose down -t0 || true
 }
 
 run() {
-    docker-compose up -d 
-    docker-compose exec -T klipper bash -c '
+    docker compose up -d 
+    docker compose exec -T klipper bash -c '
         cd /app
         ./install-led_effect.sh --no-moonraker --no-service -k /opt/klipper/ --venv /opt/venv/
         exit 0
 ';
-    docker-compose exec -T klipper bash -c '
+    docker compose exec -T klipper bash -c '
         cd /opt
         /opt/venv/bin/python klipper/klippy/klippy.py \
             -v \
@@ -28,14 +28,14 @@ run() {
     set +e
     # shellcheck disable=SC2034
     for i in {1..100}; do
-        INFO=$(docker-compose exec -T moonraker curl http://localhost:7125/printer/info)
+        INFO=$(docker compose exec -T moonraker curl http://localhost:7125/printer/info)
         # shellcheck disable=SC2181
         if [ $? -ne 0 ]; then
             sleep 1s
             continue;
         fi
 
-        docker-compose exec -T klipper bash -c 'cat printer_data/logs/klippy.log'
+        docker compose exec -T klipper bash -c 'cat printer_data/logs/klippy.log'
         if [[ "$INFO" =~ \"state\":\ *\"ready\" ]]; then
             exit 0
         fi
